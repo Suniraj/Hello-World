796f987f36c1ed569762790e04b729761630ccff
Suneetha Ragupatula <suneetha@suneethas-macbook-pro.local>
2018-02-28 16:37:46 -0600

    3

---
+++ Gittrial/Gittrial/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Gittrial
+//
+//  Created by Suneetha Ragupatula on 2/28/18.
+//  Copyright © 2018 Suneetha Ragupatula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("button Clicked...git example..")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
